e631108cb0323f025dae97b65ba3e98c00655788
bertha <bertha.bravo@davinci.edu.ar>
2023-10-04 19:36:46 -0300

    incorporacion modal detalle

--- js/app.js
+++ js/app.js
@@ -16,9 +16,7 @@ console.log('se ejecuta ultimo')
 
 
 /*
-respuestaJson= " [ {'nombre': 'pikachu', 'url': 'https://www.urlejemplo.com'},
-{'nombre': 'pikachu', 'url': 'https://www.urlejemplo.com'},{'nombre': 'pikachu', 
-'url': 'https://www.urlejemplo.com'}] ";
+respuestaJson= " [ {'nombre': 'pikachu', 'url': 'https://www.urlejemplo.com'},{'nombre': 'pikachu', 'url': 'https://www.urlejemplo.com'},{'nombre': 'pikachu', 'url': 'https://www.urlejemplo.com'}] ";
 
 respuestaJson.json();
 
@@ -50,25 +48,65 @@ fetch(`https://pokeapi.co/api/v2/pokemon/?limit=100`)
 
 
 const tableBody = document.querySelector('#tableBody');
+const myModal = new bootstrap.Modal(document.getElementById('modalPokemonData'));
 
-
-function renderTabla() {
+function renderTabla(){
     fetch(`https://pokeapi.co/api/v2/pokemon/?limit=100`)
-        .then(response => response.json())
-        .then(data => {
-            const listado = data.results.map((elemento) => {
-                return `<tr>
+    .then(response => response.json())
+    .then(data => {
+        const listado = data.results.map((elemento) => {
+            return `<tr>
                 <td>${elemento.name}</td>
                 <td>${elemento.url}</td>
-                <td><a href="" class="btn btn-outline-primary">Ver más</a></td>
+                <td>
+                <button type="button" id="${elemento.url}" class="btn btn-outline-primary btnVerMas" >
+                    Ver más
+                </button>
+                </td>
             </tr>`
-            });
+        });
 
-            tableBody.innerHTML = listado.join('');
+        tableBody.innerHTML = listado.join('');
+
+        const botones = document.querySelectorAll('.btnVerMas');
+
+        botones.forEach((btn) => {
+            btn.addEventListener('click', () => {
+                const urlApi = btn.id; 
+
+                fetch(urlApi)
+                .then( response => response.json())
+                .then(data => {
+                    console.log(data);
+                    const tituloModal = document.querySelector('.modal-title');
+                    const cuerpoModal = document.querySelector('.modal-body');
+                    tituloModal.textContent = data.name;
+
+                    const tipo = data.types.map((elemento) => {
+                        return elemento.type.name;
+                    })
+                    const habilidades = data.abilities.map((elemento) => {
+                        return elemento.ability.name;
+                    })
+
+ 
+
+                    cuerpoModal.innerHTML = `
+                        <img src="${data.sprites.front_default}" class="w-25 mx-auto d-block" alt="">
+                        <p class="text-center"><strong>Peso: </strong> ${data.weight}</p>
+                        <p class="text-center"><strong>Tipo: </strong> ${tipo.join(', ')}</p>
+                        <p class="text-center"><strong>Habilidades: </strong> ${habilidades.join(', ')}</p>
+                    `;
+                    myModal.show();
+                })
+
+            })
         })
-        .catch(error => {
-            console.error("Ha ocurrido un error:" + error)
-        })
+
+    })
+    .catch(error => {
+        console.error("Ha ocurrido un error:" + error)
+    })
 }
 
 renderTabla();
